ef4f491f83dd4a3e4ff2b12653b997efbbf56428
JoelChipoya <112080444+owellJoval@users.noreply.github.com>
2025-04-17 20:54:09 +0200

    Update IjgapisApplication.java

--- src/main/java/com/example/ijgapis/IjgapisApplication.java
+++ src/main/java/com/example/ijgapis/IjgapisApplication.java
@@ -3,11 +3,15 @@ package com.example.ijgapis;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
-public class IjgapisApplication {
-
+public class IjgApplication {
     public static void main(String[] args) {
-        SpringApplication.run(IjgapisApplication.class, args);
+        Dotenv dotenv = Dotenv.configure().load();
+        dotenv.entries().forEach(entry ->
+            System.setProperty(entry.getKey(), entry.getValue())
+        );
+        SpringApplication.run(IjgApplication.class, args);
     }
-
 }
