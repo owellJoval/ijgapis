810316433a4319d15877e3dc967833ede132df3e
JoelChipoya <112080444+owellJoval@users.noreply.github.com>
2025-06-11 14:36:16 +0200

    Update NewsController.java

--- src/main/java/com/example/ijgapis/Controllers/NewsController.java
+++ src/main/java/com/example/ijgapis/Controllers/NewsController.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/news")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://research-portal-final.vercel.app")
 public class NewsController {
 
     @Autowired
