ea105db47e56bf5bb81319b310dcac46ff105b1e
JoelChipoya <112080444+owellJoval@users.noreply.github.com>
2025-06-11 14:35:56 +0200

    Update DocumentController.java

--- src/main/java/com/example/ijgapis/Controllers/DocumentController.java
+++ src/main/java/com/example/ijgapis/Controllers/DocumentController.java
@@ -20,7 +20,7 @@ import java.util.List;
 @RestController
 //
 @RequestMapping("/api/documents")
-@CrossOrigin(origins = {"https://ijg-research-admin.vercel.app", "http://localhost:3000"},
+@CrossOrigin(origins = {"https://research-portal-final.vercel.app", "https://research-portal-final.vercel.app/"},
     allowedHeaders = "*", 
     methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, 
     exposedHeaders = {"Content-Disposition", "Content-Type"}, 
