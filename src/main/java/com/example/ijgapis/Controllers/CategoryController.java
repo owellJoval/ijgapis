8716cb0ebceb9e6176b00c3233fa1c1fc32a2fca
JoelChipoya <112080444+owellJoval@users.noreply.github.com>
2025-06-11 14:38:08 +0200

    Update CategoryController.java

--- src/main/java/com/example/ijgapis/Controllers/CategoryController.java
+++ src/main/java/com/example/ijgapis/Controllers/CategoryController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/categories")
-@CrossOrigin(origins = "https://research-portal-final.vercel.app")
+@CrossOrigin(origins = "*")
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
